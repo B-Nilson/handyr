9820419f792cedca55a9c3d1e044dafecca09b44
Brayden Nilson <nilson.brayden@gmail.com>
2025-04-23 21:58:12 -0700

    document and export for_each()

--- R/for_each.R
+++ R/for_each.R
@@ -1,3 +1,51 @@
+#' Loop over a vector and apply a function with useful postprocessing
+#'
+#' @param input A vector of values to be iterated over.
+#' @param FUN A function to be applied to each value in `input`.
+#' @param ... Any other arguments to be passed to `FUN`.
+#' @param .bind (Optional) A single logical (TRUE/FALSE) value indicating if the output should be bound rowwise.
+#'   Default is `FALSE`
+#' @param .name (Optional) A single logical (TRUE/FALSE) value indicating if the output should be named after the input.
+#'   Default is `FALSE`
+#' @param .parallel (Optional) A single logical (TRUE/FALSE) value indicating if the function should be run in parallel (see [future.apply::future_lapply()]).
+#'   Default is `FALSE`
+#' @param .workers (Optional) A single numeric value indicating the number of workers to run in parallel.
+#'   Default is all available cores (see [parallel::detectCores()])
+#' @param .invisible (Optional) A single logical (TRUE/FALSE) value indicating if the output should be invisible (no messages/warnings).
+#'
+#' @description
+#' `for_each` provides a simple way to loop over a vector and apply a function with useful postprocessing.
+#'
+#' @family Utilities
+#'
+#' @return the output of `FUN` iterated over `input`, optionally bound rowwise, named after `input`, or invisible.
+#' @export
+#'
+#' @examples
+#' values <- 1:3
+#' values |> for_each(\(value) value + 1)
+#'
+#' list(
+#'   data.frame(x = 1:3),
+#'   data.frame(x = 4:6)
+#' ) |>
+#'   for_each(
+#'     \(dat) dat |> dplyr::mutate(y = x + 1),
+#'     .bind = TRUE
+#'   )
+#'
+#' c("bread", "jam") |>
+#'   for_each(
+#'     \(value) paste("eat", value),
+#'     .name = TRUE
+#'   )
+#'
+#' values <- 1:3 |>
+#'   for_each(\(value) value + 1, .parallel = TRUE, .workers = 2)
+#'
+#' values <- 1:3 |>
+#'   for_each(\(value) message(value + 1), .invisible = TRUE)
+
 for_each = function(input, FUN, ..., .bind = FALSE, .name = FALSE, .parallel = FALSE, .workers = parallel::detectCores(), .invisible = FALSE) {
   if (.parallel) {
     future::plan(future::multisession, workers = .workers) # Run in parallel if desired
