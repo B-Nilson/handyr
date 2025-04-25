b7888431d8903b9c99fe79321f890309a52f8942
Brayden Nilson <nilson.brayden@gmail.com>
2025-04-24 21:23:52 -0700

    cleanup save_figure and add base_height arg

--- R/save_figure.R
+++ R/save_figure.R
@@ -1,16 +1,21 @@
-#' Save a ggplot2 figure
+#' Save a ggplot2 figure for publishing
 #'
 #' @param gg a single ggplot object you would like to save.
 #' @param out_path A single character value indicating where to save the plot to (i.e. "./plots/my_figure_name.png")
-#' @param taller A single numeric value indicating how many inches to add/remove to the output plot height. Default makes a 5"x6.5" image.
-#' @param page_width A single numeric value indicating the width of the page (minus margins) in inches that the image is intended for.
+#' @param page_width A single numeric value indicating the width of the page (minus margins) that the image is intended for.
+#'   Default is 6.5".
+#' @param base_height A single numeric value indicating the height of the figure (+`taller`).
+#'   Default is 5".
+#' @param taller A single numeric value indicating how much to add/remove to the plot `base_height`.
+#'   Default is 0.
+#' @param units A single character value equal to "in" (default) or "cm" indicating the units of `page_width`, `base_height`, and `taller`.
 #' @param quality A single character value equal to "high" (dpi = 300), "medium" (dpi = 200), or "low" (dpi = 100) indicating the output quality of the figure.
 #'   OR a single numeric value equal to the desired dpi.
 #'   Text sizes may need to be adjusted in relavant geoms/themes for different quality levels.
-#' @param ... (Optional) addition arguments passed on to ggplot2::ggsave()
+#' @param ... (Optional) addition arguments passed on to ggplot2::ggsave() (aside from )
 #'
 #' @description
-#' `save_figure()` is a wrapper around `ggplot2::ggsave()` that saves a high quality 5x7 figure by default.
+#' `save_figure()` is a wrapper around `ggplot2::ggsave()` that saves a high quality 5x6.5 figure by default.
 #' You can adjust the sizing from there by changing `taller` and `page_width`.
 #'
 #' @family Data Visualisation
@@ -25,14 +30,14 @@
 #'     ggplot2::aes(x, y)
 #'   )
 #' # save_figure(gg, "./test.png", taller = 1)
-save_figure <- function(gg, out_path, taller = 0, page_width = 6.5, quality = "high", ...) {
+save_figure <- function(gg, out_path, page_width = 6.5, base_height = 5, taller = 0, units = "in", quality = "high", ...) {
   dpi <- if (quality == "high") 300 else if (quality == "medium") 200 else if (quality == "low") 100 else quality
   ggplot2::ggsave(
     filename = out_path,
     plot = gg,
     width = page_width,
-    height = 5 + taller,
-    units = "in",
+    height = base_height + taller,
+    units = units,
     dpi = dpi,
     ...
   )
