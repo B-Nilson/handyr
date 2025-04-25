ee10a4119f4d0d22b94e74bf9648a64abdb6b834
Brayden Nilson <nilson.brayden@gmail.com>
2025-04-24 21:13:19 -0700

    test and cleanup rolling

--- R/rolling.R
+++ R/rolling.R
@@ -1,19 +1,20 @@
 #' Perform a rolling calculation on a vector if enough values are provided.
 #'
 #' @param x A vector of values
-#' @param FUN A function to be applied to each window of `x`.
+#' @param FUN (Optional) A function to be applied to each window of `x`.
 #'   The default is `mean`.
-#' @param ... Additional arguments to be passed to `FUN`
-#' @param .width A single numeric value indicating the width of the window.
-#' @param .direction A single character string or numeric value indicating the direction of the window.
+#' @param ... (Optional) Additional arguments to be passed to `FUN`
+#' @param .width (Optional) A single numeric value indicating the width of the window.
+#'   The default is `3`.
+#' @param .direction (Optional) A single character string or numeric value indicating the direction of the window.
 #'   The default is `"backward"`.
 #'   Options are `"backward"` (1), `"forward"` (-1), or `"center"` (0).
-#' @param .fill A single value to be used for filling in any values that are outside of the window.
+#' @param .fill (Optional) A single value to be used for filling in any values that are outside of the window.
 #'   The default is `NA`.
-#' @param .min_length A single numeric value indicating the minimum number of observations required to
+#' @param .min_length (Optional) A single numeric value indicating the minimum number of observations required to
 #'   compute a value.
 #'   The default is `0`.
-#' @param .digits A single numeric value indicating the number of decimal places to round the output.
+#' @param .digits (Optional) A single numeric value indicating the number of decimal places to round the output.
 #'   The default is `0`.
 #'
 #' @examples
@@ -23,7 +24,7 @@
 #' @export
 # TODO: code without zoo (use dplyr::lag/lead)
 # TODO: document, test, and export
-rolling <- function(x, FUN = mean, ..., .width, .direction = "backward", .fill = NA, .min_length = 0, .digits = 0) {
+rolling <- function(x, FUN = mean, ..., .width = 3, .direction = "backward", .fill = NA, .min_length = 0, .digits = 0) {
   align <- ifelse(
     .direction %in% c("backward", 1), "right",
     ifelse(.direction %in% c("forward", -1), "left",
@@ -32,8 +33,8 @@ rolling <- function(x, FUN = mean, ..., .width, .direction = "backward", .fill =
   )
   x |>
     zoo::rollapply(
-      width = .width, align = .align, fill = .fill,
+      width = .width, align = align, fill = .fill,
       FUN = \(x) do_if_enough(x, FUN, .min_length = .min_length)
     ) |>
-    round(digits = digits)
+    round(digits = .digits)
 }
---
+++ tests/testthat/test-rolling.R
@@ -0,0 +1,41 @@
+test_that("basic case works", {
+  expect_no_warning(expect_no_error(
+    (1:5) |> rolling(mean)
+  ))
+})
+
+test_that("FUN works", {
+  expect_equal(expect_no_warning(expect_no_error(
+    (1:5) |> rolling(sum) |> sum(na.rm = TRUE)
+  )), expected = 27)
+})
+
+test_that(".width works", {
+  expect_equal(expect_no_warning(expect_no_error(
+    (1:5) |> rolling(mean, .width = 4) |> sum(na.rm = TRUE)
+  )), expected = 6)
+})
+
+test_that(".direction works", {
+  expect_no_warning(expect_no_error(
+    (1:5) |> rolling(mean, .direction = "forward")
+  ))
+})
+
+test_that(".fill works", {
+  expect_equal(expect_no_warning(expect_no_error(
+    (1:5) |> rolling(mean, .fill = -1) |> sum()
+  )), expected = 7)
+})
+
+test_that(".min_length works", {
+  expect_equal(expect_no_warning(expect_no_error(
+    c(1:3, NA_real_) |> rolling(mean, .min_length = 4) |> sum()
+  )), expected = NA_real_)
+})
+
+test_that(".digits works", {
+  expect_equal(expect_no_warning(expect_no_error(
+    (1:3 * exp(5)^2) |> rolling(mean, .digits = 1)
+  )), expected = c(NA, NA, 44052.9))
+})
