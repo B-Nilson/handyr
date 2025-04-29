554c8f52ddace1805729899b01e8cf430110ae68
Brayden Nilson <nilson.brayden@gmail.com>
2025-04-28 21:58:36 -0700

    cleanup

--- R/log.R
+++ R/log.R
@@ -1,10 +1,10 @@
 #' Log a message with optional timestamp and formatting
 #'
 #' @param message A character value of the message to be logged
+#' @param header A logical value indicating if the message should be formatted as a header
 #' @param time A logical value indicating if the current time should be prepended to the log message
 #' @param time_format A character value indicating the format of the timestamp
 #' @param tz A character value indicating the time zone to use for the timestamp
-#' @param header A logical value indicating if the message should be formatted as a header
 #' @param quiet A logical value indicating if the message should be printed
 #'
 #' @return a character vector of the formatted message
@@ -12,12 +12,12 @@
 #' @examples
 #' log("My Awesome Script", time = FALSE, header = TRUE)
 #' log("Step 1...")
-#' Sys.sleep(0.8)
+#' # Do something
 #' log("Step 2...")
-#' log("Complete", time = FALSE, header = TRUE)
-log <- function(message, time = TRUE, time_format = "%Y-%m-%d %H:%M:%S", tz = NULL, header = FALSE, quiet = FALSE) {
-  original <- message
-
+#' # Do something else
+#' log("Complete")
+# TODO: rename to log_step due to overlap with base::log()
+log <- function(message, header = FALSE, time = !header, time_format = "%Y-%m-%d %H:%M:%S", tz = NULL, quiet = FALSE) {
 
   stopifnot(is.character(message))
   stopifnot(is.logical(header), length(header) == 1)
@@ -26,17 +26,24 @@ log <- function(message, time = TRUE, time_format = "%Y-%m-%d %H:%M:%S", tz = NU
   stopifnot(is.character(tz) | is.null(tz), length(tz) == 1 | is.null(tz))
   stopifnot(is.logical(quiet), length(quiet) == 1)
 
+  message <- paste(message, collapse = " ")
+  original <- message
+  
   # Get current timestamp
   if (is.null(tz)) tz <- Sys.timezone()
   timestamp <- Sys.time() |>
     format(time_format, tz = tz)
 
   # Prepend timestamp if desired
-  if (time) message <- paste0(timestamp, ": ", message)
+  if (time) {
+    message <- timestamp |>
+      paste0(": ", message)
+  }
 
   # Format as header if desired
   if (header) {
-    n_dashes <- clamp(nchar(message) * 4, c(0, 20))
+    n_dashes <- (nchar(message) * 4) |>
+      clamp(range = c(0, 20)) # TODO: allow for user control
     line <- rep("-", n_dashes) |> paste(collapse = "")
     message <- paste0("|", line, "   ", message, "   ", line, "|")
   }
