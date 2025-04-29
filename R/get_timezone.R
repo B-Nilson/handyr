8cbca0c30b731f63bb7d09de028beefe56ef6e5d
Brayden Nilson <nilson.brayden@gmail.com>
2025-04-28 21:57:50 -0700

    add example plus quiet argument to supress fast method warning

--- R/get_timezone.R
+++ R/get_timezone.R
@@ -3,7 +3,8 @@
 #' @param lng Vector of numeric values indicating location longitudes (decimal degrees) to lookup.
 #' @param lat Vector of numeric values indicating location latitudes (decimal degrees) to lookup.
 #' @param method A single character value indicating the lookup method to use (either "accurate" or "fast"). See [lutz::tz_lookup_coords] for more details.
-#' @param ... (Optional) Additional paramaters to pass to [lutz::tz_lookup_coords]
+#' @param quiet A single logical value indicating if messages and warnings should be suppressed.
+#' @param ... Additional paramaters to pass to [lutz::tz_lookup_coords] (currently unused)
 #'
 #' @description
 #' `get_timezone` uses the `lutz` package to lookup the timezone of locations from lat/lng coords. See [lutz::tz_lookup_coords] for more details.
@@ -14,8 +15,9 @@
 #' @export
 #'
 #' @examples
-#' get_timezone(-105.053144, 69.116178, method = "fast")
-get_timezone <- function(lng, lat, method = "accurate", ...) {
+#' get_timezone(lng = -105.053144, lat = 69.116178, method = "accurate")
+#' get_timezone(lng = c(-105.053144, -106.053144), lat = c(69.116178, 49.116178), method = "fast")
+get_timezone <- function(lng, lat, method = "accurate", quiet = FALSE, ...) {
   rlang::check_installed("lutz")
   # Handle inputs
   stopifnot(is.numeric(lng), is.numeric(lat))
@@ -27,6 +29,8 @@ get_timezone <- function(lng, lat, method = "accurate", ...) {
   lutz::tz_lookup_coords(
     lat = lat,
     lon = lng,
-    method = method, ...
+    method = method,
+    warn = !quiet,
+    ...
   )
 }
