0132dd3119f546438959824e487b1d159fe150c0
Brayden Nilson <nilson.brayden@gmail.com>
2025-04-29 23:27:13 -0700

    improve get_timezone documentation

--- R/get_timezone.R
+++ R/get_timezone.R
@@ -1,17 +1,17 @@
-#' Get timezones of latitude/longitude pairs
+#' Lookup timezones of latitude/longitude pairs
 #'
-#' @param lng Vector of numeric values indicating location longitudes (decimal degrees) to lookup.
-#' @param lat Vector of numeric values indicating location latitudes (decimal degrees) to lookup.
-#' @param method A single character value indicating the lookup method to use (either "accurate" or "fast"). See [lutz::tz_lookup_coords] for more details.
-#' @param quiet A single logical value indicating if messages and warnings should be suppressed.
-#' @param ... Additional paramaters to pass to [lutz::tz_lookup_coords] (currently unused)
-#'
-#' @description
 #' `get_timezone` uses the `lutz` package to lookup the timezone of locations from lat/lng coords. See [lutz::tz_lookup_coords] for more details.
+#' 
+#' @param lng,lat Numeric longitude/latitude pairs (decimal degrees).
+#' @param method either "accurate" or "fast",  indicating the lookup method to use. 
+#'   See [lutz::tz_lookup_coords] for more details.
+#'   Set `.quiet` to `TRUE` to suppress warning produced when `method = "fast"`.
+#' @param quiet A logical value indicating if messages and warnings should be suppressed.
+#' @param ... Additional parameters to pass to [lutz::tz_lookup_coords] (currently unused)
 #'
-#' @family Utilities
-#'
-#' @return a character vector with the same length as `lat` and `lng` indicating the locations likely timezone.
+#' @return a character vector with the same length as `lat` and `lng` indicating each locations likely timezone.
+#'  See [lutz::tz_lookup_coords] for more details.
+#' 
 #' @export
 #'
 #' @examples
