5c34ee3b79df48f968b3e0b7d71620d8a6453137
Brayden Nilson <nilson.brayden@gmail.com>
2025-04-29 21:24:41 -0700

    improve clamp documentation

--- R/clamp.R
+++ R/clamp.R
@@ -1,12 +1,18 @@
-#' Clamp a value to a certain range
+#' Replace values outside of a range with the nearest value in that range
+#' 
+#' `clamp` sets values less than `min(range)` to `min(range)` and values greater than `max(range)` to `max(range)`
 #'
-#' @param x A numeric vector of value(s)
-#' @param range A numeric vector of length two to clamp `x` to. If `range[1]` is `NA`, then `min(x)` will be used. If `range[2]` is `NA`, then `max(x)` will be used
-#' @return A numeric vector of `x` with values less than `min(range)` set to `min(range)` and values greater than `max(range)` set to `max(range)`
+#' @param x A numeric vector.
+#' @param range A numeric vector of length two to clamp `x` to. 
+#'   * If `range[1]` is `NA` (default), then `min(x)` will be used. 
+#'   * If `range[2]` is `NA` (default), then `max(x)` will be used.
+#' @return A numeric vector equal to `x` with:
+#'  * values less than `min(range)` set to `min(range)`
+#'  * values greater than `max(range)` set to `max(range)`
 #'
 #' @examples
-#' clamp(1:5, range = c(2, 4))
-#' clamp(1:5, range = c(-1, 8))
+#' clamp(1:5, range = c(2, 4)) # 2, 2, 3, 4, 4
+#' clamp(1:5, range = c(-1, 1)) # 1, 1, 1, 1, 1
 #' @export
 clamp <- function(x, range = c(NA, NA)) {
   # Handle inputs
