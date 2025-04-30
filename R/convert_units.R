41bf5422d33605f627485167bf8ffdb3a360cc81
Brayden Nilson <nilson.brayden@gmail.com>
2025-04-29 21:57:53 -0700

    improve convert_units documentation

--- R/convert_units.R
+++ R/convert_units.R
@@ -1,26 +1,29 @@
 #' Convert between common units
 #'
-#' @param x A numeric vector to convert
-#' @param from A string indicating the current unit of `x`. Examples: "cm", "mm", "in", "ft", "m", "km", "mi", "yd", "nm", "mi", "mile", "km", "mi", "yd", "ft", "in", "mm", "cm"
-#' @param to A string indicating the desired unit of `x`. Examples: "cm", "mm", "in", "ft", "m", "km", "mi", "yd", "nm", "mi", "mile", "km", "mi", "yd", "ft", "in", "mm", "cm"
+#' `convert_units` is a wrapper around [units::set_units()] to switch between common measurement units.
+#'
+#' @param x A numeric vector.
+#' @param from,to A string indicating the current/desired unit of `x`.
+#'  Run [units::valid_udunits()] for a list of valid units.
 #' @param ... Additional arguments passed on to [units::set_units()]
-#' @description A wrapper around [units::set_units()] to convert between common units
-#'  Run [units::valid_udunits()] for a list of valid units
-#' @return A numeric vector of `x` converted to the desired unit
+#'
+#' @return A numeric vector with the same length/type as `x`.
+#'
 #' @export
+#'
 #' @examples
 #' convert_units(c(1, 2, 3), from = "cm", to = "km")
 #' convert_units(c(1, 2, 3), from = "degF", to = "degC")
 #' convert_units(c(1, 2, 3), from = "ft", to = "m")
 #' convert_units(c(1, 2, 3), from = "oz", to = "L")
 #' convert_units(c(1, 2, 3), from = "m/s", to = "km/h")
-#' convert_units(c(1, 2, 3), from = "ug/m3", to = "g/km3")
+#' convert_units(c(1, 2, 3), from = "ug/m3", to = "mg/km3")
 convert_units <- function(x, from, to, ...) {
   rlang::check_installed("units")
   # handle inputs
   stopifnot(is.numeric(x), is.character(from), is.character(to))
   stopifnot(length(from) == 1, length(to) == 1)
-  
+
   # convert units
   x |>
     units::set_units(from, mode = "standard", ...) |>
