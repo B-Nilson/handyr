e024ee9d07e729367ea09c880f8e6bd5286c6793
Brayden Nilson <nilson.brayden@gmail.com>
2025-04-29 22:32:37 -0700

    add return value control (.return) and cleanup documentation

--- R/do_if_enough.R
+++ R/do_if_enough.R
@@ -1,21 +1,29 @@
-#' Apply a function if enough values are provided
-#'
-#' @param x A vector of values
-#' @param FUN A function to be applied to x
-#' @param ... Additional arguments to be passed to FUN
-#' @param .min_non_na The minimum length of `x` (ignoring NAs) to be considered enough
+#' Apply a function if enough values are non-NA
 #'
 #' @description
-#'   `do_if_enough` provides a simple way to apply a function if enough values are provided.
+#'   `do_if_enough`:
+#'    * checks if enough non-NA values are in `x`. (see `.min_non_na`)
+#'    * if so, applies a function (`FUN`) to `x`.
+#'    * if not, returns `.return`.
+#' 
+#' @param x A vector (numeric, character, etc).
+#' @param FUN A function to be applied to `x`.
+#' @param ... Additional arguments to be passed to `FUN`.
+#' @param .min_non_na The minimum length of `x` (ignoring NAs) to be considered enough.
+#' @param .return What to return instead of the output of `FUN` if not enough non-NA values.
+#'
 #' @return The output of FUN applied to x if enough values are provided, NA otherwise
+#' 
 #' @export
 #'
 #' @examples
 #' do_if_enough(c(1, 2, 3), mean, .min_non_na = 2)
 #' do_if_enough(c(1, 2, NA), mean, .min_non_na = 2)
 #' do_if_enough(c(1, NA, NA), mean, .min_non_na = 2)
-do_if_enough <- function(x, FUN, ..., .min_non_na = 1) {
+#' do_if_enough(c(1, NA, NA), mean, .min_non_na = 2, .return = -1)
+do_if_enough <- function(x, FUN, ..., .min_non_na = 1, .return = NA) {
   # Handle inputs
+  stopifnot(is.vector(x), !is.list(x))
   stopifnot(is.function(FUN))
   stopifnot(
     is.numeric(.min_non_na), length(.min_non_na) == 1, .min_non_na >= 0,
@@ -26,10 +34,10 @@ do_if_enough <- function(x, FUN, ..., .min_non_na = 1) {
   if (.min_non_na <= 1) {
     return(FUN(x, na.rm = TRUE, ...))
   }
-  # Return NA if too few values
+  # Return .return if too few values
   is_enough <- sum(!is.na(x)) >= .min_non_na
   if (!is_enough) {
-    return(NA)
+    return(.return)
   }
   
   # Apply function ignoring NAs
