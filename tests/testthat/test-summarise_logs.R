abe742d8c700244a06a57b1758492069919dd3fd
Brayden Nilson <nilson.brayden@gmail.com>
2025-04-30 21:28:53 -0700

    drop first AND last row of log sections (first is headers, last is footer)

--- tests/testthat/test-summarise_logs.R
+++ tests/testthat/test-summarise_logs.R
@@ -1,11 +1,12 @@
 test_that("basic case works", {
   logs <- list(
-    log_step("test", quiet = TRUE),
-    log_step("test", quiet = TRUE),
-    log_step("test", quiet = TRUE),
-    log_step("test", quiet = TRUE),
-    log_step("test", quiet = TRUE),
-    log_step('test', quiet = TRUE)
+    log_step("Header", header = TRUE, quiet = TRUE),
+    log_step("test1", quiet = TRUE),
+    log_step("test2", quiet = TRUE),
+    log_step("test3", quiet = TRUE),
+    log_step("test4", quiet = TRUE),
+    log_step("test5", quiet = TRUE),
+    log_step('Complete', quiet = TRUE)
   )
   expect_message(
     summarise_logs(logs)
