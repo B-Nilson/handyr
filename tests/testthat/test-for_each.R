bf557265e98cf1ab41b682b8b7af7070ab4920c2
Brayden Nilson <nilson.brayden@gmail.com>
2025-05-01 20:14:14 -0700

    improve for_each tests

--- tests/testthat/test-for_each.R
+++ tests/testthat/test-for_each.R
@@ -1,8 +1,9 @@
 test_that("basic case works", {
   values <- 1:3
-  expect_no_warning(expect_no_error(
-    values |> for_each(\(value) value + 1)
-  ))
+  expect_equal(
+    values |> for_each(\(value) value + 1),
+    expected = 2:4
+  )
 })
 
 test_that(".bind works", {
@@ -10,36 +11,46 @@ test_that(".bind works", {
     data.frame(x = 1:3),
     data.frame(x = 4:6)
   )
-  expect_no_warning(expect_no_error(
+  expect_equal(
     dats |>
       for_each(
         \(dat) dat |> dplyr::mutate(y = x + 1),
         .bind = TRUE
-      )
-  ))
+      ),
+    expected = data.frame(
+      x = 1:6,
+      y = 2:7
+    )
+  )
 })
 
 test_that(".name works", {
-  values <- c("bread", "jam")
-  expect_no_warning(expect_no_error(
-    values |>
+  expect_equal(
+    c("bread", "jam") |>
       for_each(
-        \(value) paste("hello", value),
+        \(value) paste("eat", value),
         .name = TRUE
-      )
-  ))
+      ),
+    expected = list(bread = "eat bread", jam = "eat jam")
+  )
 })
 
 test_that(".parallel/.workers works", {
   values <- 1:3
-  expect_no_warning(expect_no_error(
-    values |> for_each(\(value) value + 1, .parallel = TRUE, .workers = 2)
-  ))
+  expect_equal(
+    values |> for_each(\(value) value + 1, .parallel = TRUE),
+    expected = 2:4 |> as.list()
+  )
+  expect_equal(
+    values |> for_each(\(value) value + 1, .parallel = TRUE, .workers = 2),
+    expected = 2:4 |> as.list()
+  )
 })
 
 test_that(".quiet works", {
   values <- 1:3
-  expect_no_warning(expect_no_message(expect_no_error(expect_invisible(
-    values |> for_each(\(value) message(value + 1), .quiet = TRUE)
-  ))))
+  expect_no_message(expect_invisible(expect_equal(
+    values |> for_each(\(value) {message(value + 1); value + 1}, .quiet = TRUE),
+    expected = 2:4 |> as.list()
+  )))
 })
