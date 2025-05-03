42ff7d8a63da9fed45ad7092fbf87b9b0d8612bf
Brayden Nilson <nilson.brayden@gmail.com>
2025-05-02 19:10:17 -0700

    add save_to tests

--- tests/testthat/test-summarise_logs.R
+++ tests/testthat/test-summarise_logs.R
@@ -10,5 +10,21 @@ test_that("basic case works", {
   )
   expect_message(
     summarise_logs(logs)
+  ) |>
+    expect_snapshot()
+})
+
+test_that("save_to works", {
+  logs <- list(
+    log_step("Header", header = TRUE, quiet = TRUE),
+    log_step("test1", quiet = TRUE),
+    log_step("test2", quiet = TRUE),
+    log_step("test3", quiet = TRUE),
+    log_step("test4", quiet = TRUE),
+    log_step("test5", quiet = TRUE),
+    log_step("Complete", quiet = TRUE)
   )
+  temp_file <- tempfile()
+  expect_message(summarise_logs(logs, save_to = temp_file))
+  expect_true(file.exists(temp_file))
 })
