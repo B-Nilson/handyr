8668da8d7d3d3577c9d6397f4530889714db5751
Brayden Nilson <nilson.brayden@gmail.com>
2025-05-07 22:16:03 -0700

    add silence function

---
+++ R/silence.R
@@ -0,0 +1,61 @@
+#' Silence unwanted output
+#'
+#' This function wraps `tryCatch` and suppresses some or all of the following:
+#' - errors
+#' - warnings
+#' - messages
+#' - output from `print()`
+#' - output from `cat()`
+#' - output from `writeLines()`
+#'
+#' @param this_please Some code to run silently. 
+#'   If running multiple lines use `{....}`
+#' @param errors A logical value indicating if errors should be silenced.
+#'   Default is `TRUE`.
+#' @param warnings A logical value indicating if warnings should be silenced.
+#'   Default is `TRUE`.
+#' @param messages A logical value indicating if messages should be silenced.
+#'   Default is `TRUE`.
+#' @param output A logical value indicating if output from `print()`, `cat()`, and `writeLines()` should be silenced.
+#'   Default is `TRUE`.
+#' 
+#' @return The output from `this_please`
+#' @export
+#' 
+#' @examples
+#' silence(1/0)
+#' silence(warning("test"))
+#' silence(message("test"))
+#' silence(print("test"))
+#' silence(cat("test"))
+#' silence(1 + "a")
+silence <- function(this_please, errors = TRUE, warnings = TRUE, messages = TRUE, output = TRUE) {
+  # Functions for each level of silence
+  silencers <- list(
+    errors = suppressErrors,
+    warnings = suppressWarnings,
+    messages = suppressMessages,
+    output = capture.output
+  )
+  # Filter to desired silencers
+  enabled_silencers <- silencers[
+    which(c(errors, warnings, messages, output))
+  ]
+  # Run invisibly if no silencers
+  if (length(enabled_silencers) == 0) {
+    return(invisible(eval(to_run)))
+  }
+  # Build a string that wraps each silencer around `this_please`
+  closing_parens <- ")" |>
+    rep(length(enabled_silencers)) |> 
+    paste(collapse = "")
+  silencer <- paste0(
+    "enabled_silencers[[", 
+    seq_along(enabled_silencers), collapse = "",
+    "]]("
+  ) |> paste0("this_please", closing_parens)
+  # Evaluate that string invisibly
+  invisible(eval(parse(text = silencer)))
+}
+
+suppressErrors <- function(...) try(..., silent = TRUE)
