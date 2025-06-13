7da72acc837f9507b424c5c57923e1b7ae699174
B-Nilson <B-Nilson@users.noreply.github.com>
2025-06-13 05:55:26 +0000

    Style code (GHA)

--- tests/testthat/test-silence.R
+++ tests/testthat/test-silence.R
@@ -1,16 +1,19 @@
 test_that("basic case works", {
   expect_equal(expect_no_error(expect_invisible(expect_no_message(
-    capture_output(silence({
-      warning("test warning")
-      message("test message")
-      print("test print")
-      cat("test cat\n")
-      writeLines("test writeLines")
-    }, output = TRUE))
+    capture_output(silence(
+      {
+        warning("test warning")
+        message("test message")
+        print("test print")
+        cat("test cat\n")
+        writeLines("test writeLines")
+      },
+      output = TRUE
+    ))
   ))), expected = "")
 
   expect_equal(
-    silence("test"), 
+    silence("test"),
     expected = "test"
   )
 })
