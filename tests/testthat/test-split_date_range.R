02c2a05e354bdb4a0a5263e311a3e2b8b11997ea
B-Nilson <nilson.brayden@gmail.com>
2025-07-01 20:54:07 -0700

    use as.Date instead of lubridate::ymd

--- tests/testthat/test-split_date_range.R
+++ tests/testthat/test-split_date_range.R
@@ -1,6 +1,5 @@
 test_that("basic case works", {
-  date_range <- c("2020-01-01", "2020-01-12") |>
-    lubridate::ymd()
+  date_range <- as.Date(c("2020-01-01", "2020-01-12"))
   expect_snapshot(
     split_date_range(date_range, max_duration = "3 days")
   )
