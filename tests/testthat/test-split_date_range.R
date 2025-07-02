fd21b305cfad2e20b667c8d9e09c3e5b9fd97a6d
B-Nilson <nilson.brayden@gmail.com>
2025-07-01 20:04:09 -0700

    test split_date_range

---
+++ tests/testthat/test-split_date_range.R
@@ -0,0 +1,7 @@
+test_that("basic case works", {
+  date_range <- c("2020-01-01", "2020-01-12") |>
+    lubridate::ymd()
+  expect_snapshot(
+    split_date_range(date_range, max_duration = "3 days")
+  )
+})
