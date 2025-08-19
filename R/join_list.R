90040954a5a0f261eeb00e52259411fcb0dd73a2
B-Nilson <nilson.brayden@gmail.com>
2025-08-19 15:23:09 -0700

    expand on documentation for by arg default

--- R/join_list.R
+++ R/join_list.R
@@ -4,6 +4,7 @@
 #'
 #' @param df_list A list of data frames.
 #' @param by A character vector of column names to join on.
+#'   Default (NULL) joins on all matching columns.
 #' @param mode A character string specifying the type of join to perform. Must be one of "full", "inner", "left", "right", ... (see [dplyr::join]).
 #'
 #' @return A single data frame containing the joined data.
