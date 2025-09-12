18de88a27bd10bc7656f36e4d52075608050e571
B-Nilson <nilson.brayden@gmail.com>
2025-09-12 10:16:58 -0700

    cleanup

--- R/get_season.R
+++ R/get_season.R
@@ -3,29 +3,35 @@ get_season <- function(dates = Sys.time(), use_autumn = FALSE) {
   stopifnot(length(dates) >= 1)
   stopifnot(is.logical(use_autumn), length(use_autumn) == 1)
   # TODO: default use_autumn based on system local?
-  # Need at least 2 for arr.ind to work right
+
+  # Define which months are in each season
+  months_in_seasons <- list(
+    Spring = 3:5,
+    Summer = 6:8,
+    Fall = 9:11,
+    Winter = c(12, 1, 2)
+  )
+
+  # Need at least 2 dates for arr.ind to work right
   if (length(dates) == 1) {
     drop_last_date <- TRUE
     dates <- c(dates, Sys.time())
   } else {
     drop_last_date <- FALSE
   }
-  months_in_seasons <- list(
-    Spring = c(3, 4, 5),
-    Summer = c(6, 7, 8),
-    Fall = c(9, 10, 11),
-    Winter = c(12, 1, 2)
-  )
 
+  # Use Autumn instead of Fall if requested
   if (use_autumn) {
     names(months_in_seasons)[3] <- "Autumn"
   }
 
+  # Determine which season each date is in
   which_season <- months_in_seasons |>
     sapply(\(months) lubridate::month(dates) %in% months) |>
     which(arr.ind = TRUE)
-
   seasons <- names(months_in_seasons)[which_season[, 2]]
+
+  # Remove placeholder date if inserted earlier
   if (drop_last_date) {
     seasons <- seasons[-length(seasons)]
   }
