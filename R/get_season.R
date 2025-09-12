1282e8c06d49f17cf131a464491a24adfeb7d1a3
B-Nilson <nilson.brayden@gmail.com>
2025-09-11 22:48:45 -0700

    add basic function

---
+++ R/get_season.R
@@ -0,0 +1,30 @@
+get_season <- function(dates = Sys.time(), use_autumn = FALSE) {
+  # TODO: default use_autumn based on system local?
+  # Need at least 2 for arr.ind to work right
+  if (length(dates) == 1) {
+    drop_last_date <- TRUE
+    dates <- c(dates, Sys.time())
+  } else {
+    drop_last_date <- FALSE
+  }
+  months_in_seasons <- list(
+    Spring = c(3, 4, 5),
+    Summer = c(6, 7, 8),
+    Fall = c(9, 10, 11),
+    Winter = c(12, 1, 2)
+  )
+
+  if (use_autumn) {
+    names(months_in_seasons)[3] <- "Autumn"
+  }
+
+  which_season <- months_in_seasons |>
+    sapply(\(months) lubridate::month(dates) %in% months) |>
+    which(arr.ind = TRUE)
+
+  seasons <- names(months_in_seasons)[which_season[, 2]]
+  if (drop_last_date) {
+    seasons <- seasons[-length(seasons)]
+  }
+  return(seasons)
+}
