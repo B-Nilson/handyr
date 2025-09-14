7e563f91232b8de3b9e3c9f356ab7981c5b23f14
B-Nilson <nilson.brayden@gmail.com>
2025-09-14 09:06:12 -0700

    cleanup create database

--- R/create_database.R
+++ R/create_database.R
@@ -1,28 +1,44 @@
 create_database <- function(
   name,
-  type = "sqlite",
-  path = "./",
-  return_connection = FALSE
+  type = NULL,
+  path = NULL,
+  return_connection = TRUE
 ) {
   # Handle inputs
   stopifnot(is.character(name), length(name) == 1)
-  stopifnot(is.character(type), length(type) == 1)
+  stopifnot(is.character(type) & length(type) == 1 | is.null(type))
+  stopifnot(type %in% .dbi_creatable | is.null(type))
   stopifnot(is.character(path), length(path) == 1)
-  stopifnot(type %in% names(.dbi_drivers))
+
+  # If no type specified, use file extension - if none assume sqlite
+  if (is.null(type)) {
+    type <- tools::file_ext(name) |>
+      swap("", with = "sqlite")
+  }
 
   # Check if driver package is installed, prompt to install if not
-  rlang::check_installed(names(.dbi_drivers[[type]]))
+  db_driver <- .dbi_drivers[[type]]
+  rlang::check_installed(names(db_driver))
 
   # Create database directory if it doesn't exist
   if (!dir.exists(path)) {
     dir.create(path, recursive = TRUE)
   }
 
+  # Use name/path/type as needed to build database path
+  file_name <- (tools::file_ext(name) == "") |>
+    ifelse(
+      yes = paste0(name, ".", type),
+      no = name
+    )
+  database_path <- is.null(path) |>
+    ifelse(
+      yes = file_name,
+      no = paste0(path, "/", file_name)
+    )
+  
   # Create database if it doesn't exist, connect otherwise
-  database_file <- stringr::str_detect(name, "\\.") |>
-    ifelse(yes = name, no = paste0(name, ".", type))
-  database_path <- paste0(path, "/", database_file)
-  db <- .dbi_drivers[[type]][[1]]() |>
+  db <- db_driver() |>
     DBI::dbConnect(database_path)
 
   # Return connection or path to db
@@ -33,6 +49,7 @@ create_database <- function(
   return(database_path)
 }
 
+.dbi_creatable <- c("sqlite", "duckdb") # not all drivers are creatable by R
 .dbi_drivers <- list(
   sqlite = c("RSQLite" = \() RSQLite::SQLite()),
   duckdb = c("duckdb" = \() duckdb::duckdb())
