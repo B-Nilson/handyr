028b1e274e825f560a0560b0374d387b0e58120d
B-Nilson <nilson.brayden@gmail.com>
2025-09-14 10:57:01 -0700

    add functions to setup and connect to local postgresql database for windows

--- R/create_database.R
+++ R/create_database.R
@@ -9,6 +9,13 @@
 #'   If NULL (the default), the type will be derived from `name`, defaulting to "sqlite".
 #' @param path The location to save the database file to. 
 #'   If NULL (the default), the path will be derived from `name`, defaulting to the current working directory.
+#' @param version The version of the database to create. Currently only used for `type = "postgresql"`.
+#'   Default is `NULL` which uses `"17.0-1"`.
+#' @param credentials A vector of length 2 containing the username and password for the database.
+#'   Currently only used for `type = "postgresql"`.
+#'   Default is `c("USER", "PASSWORD")`.
+#' @param port The port to use for the database server. Currently only used for `type = "postgresql"`.
+#'   Default is `5432`.
 #' @param return_connection A logical value indicating whether to return the database connection instead of the path to the database file.
 #'   Default is `TRUE`.
 #' @export
@@ -16,6 +23,9 @@ create_database <- function(
     name,
     type = NULL,
     path = NULL,
+    version = NULL,
+    credentials = c("USER", "PASSWORD"),
+    port = 5432,
     return_connection = TRUE) {
   # Handle inputs
   stopifnot(is.character(name), length(name) == 1, name != "")
@@ -34,6 +44,31 @@ create_database <- function(
   db_driver <- .dbi_drivers[[type]]
   rlang::check_installed(names(db_driver))
 
+  # For  postgresql databases ---------------
+  # TODO: handle non-windows installs
+  # TODO: allow for path to be adjusted?
+  # TODO: allow for path to data be returned?
+  # TODO: allow for non-local server?
+  if (type == "postgresql") {
+    name |>
+      setup_postgres_db(
+        version = version,
+        path = path,
+        user = credentials[1],
+        password = credentials[2],
+        port = port
+      )
+    db <- name |>
+      connect_to_postgres_db(
+        version = version,
+        user = credentials[1],
+        password = credentials[2],
+        port = port
+      )
+    return(db)
+  }
+
+  # For sqlite and duckdb databases --------------
   # Create database directory if it doesn't exist
   if (!is.null(path)) {
     if (!dir.exists(path)) {
@@ -68,5 +103,158 @@ create_database <- function(
 .dbi_creatable <- c("sqlite", "duckdb") # not all drivers are creatable by R
 .dbi_drivers <- list(
   sqlite = c("RSQLite" = \() RSQLite::SQLite()),
-  duckdb = c("duckdb" = \() duckdb::duckdb())
+  duckdb = c("duckdb" = \() duckdb::duckdb()),
+  postgresql = c("RPostgres" = \() RPostgres::Postgres())
 )
+
+setup_postgres_db <- function(
+  db_name = "mydb",
+  version = NULL,
+  port = 5432,
+  user = "postgres",
+  password = "postgres"
+) {
+  if (is.null(version)) {
+    version <- "17.0-1"
+  }
+  # TODO: if user/pass are NULL, prompt user to enter
+  # TODO: handle non-windows installs
+  if (!.Platform$OS.type == "windows") {
+    stop("This function currently only supports Windows.")
+  }
+  # Define persistent folders
+  base_dir <- rappdirs::user_data_dir("postgres_handyr") |>
+    file.path(version)
+  bin_dir <- file.path(base_dir, "pgsql", "bin")
+  zip_name <- sprintf("postgresql-%s-windows-x64-binaries.zip", version)
+  zip_path <- file.path(base_dir, zip_name)
+  zip_src <- paste0("https://get.enterprisedb.com/postgresql/", zip_name)
+  data_dir <- file.path(base_dir, "data", db_name)
+  initdb <- file.path(bin_dir, "initdb.exe")
+
+  # Download binaries zip if needed
+  if (!file.exists(zip_path)) {
+    log_step("Downloading PostgreSQL binaries...")
+    # Ensure base directory exists
+    base_dir |>
+      dir.create(recursive = TRUE, showWarnings = FALSE)
+    # Prompt user to confirm download
+    response <- readline(
+      prompt = paste0(
+        "The PostgreSQL binaries (~340MB) will be downloaded.\n",
+        "Source: https://get.enterprisedb.com/postgresql/\n",
+        "Is it okay to proceed? (yes/no): "
+      )
+    )
+    if (response != "yes") {
+      stop("Download cancelled.")
+    }
+    # Download zip
+    download.file(
+      url = zip_src,
+      destfile = zip_path,
+      mode = "wb"
+    )
+  }
+
+  # Unpack binaries if needed
+  if (!file.exists(initdb)) {
+    log_step("Unpacking PostgreSQL binaries...")
+    unzip(zip_path, exdir = base_dir)
+  }
+
+  # Initialize database
+  if (!dir.exists(data_dir)) {
+    log_step("Initializing PostgreSQL database…")
+    # Ensure data directory exists
+    data_dir |> dir.create(recursive = TRUE, showWarnings = FALSE)
+    # Create temp. password file to pass to initdb
+    pwfile <- tempfile("pgpw_")
+    writeLines(password, pwfile)
+    # Run initialize database command
+    args <- "-A md5" |>
+      c("-U", user, "-D", data_dir, "--pwfile", pwfile)
+    initdb |> system2(args = args, wait = TRUE)
+    # remove temp. password file
+    file_removed <- file.remove(pwfile)
+  }
+  invisible()
+}
+
+start_postgres_server <- function(
+  db_name = "mydb",
+  version = "15.3-1",
+  port = 5432,
+  user = "postgres",
+  password = "postgres"
+) {
+  # Define persistent folders
+  base_dir <- rappdirs::user_data_dir("postgres_handyr") |>
+    file.path(version)
+  data_dir <- file.path(base_dir, "data", db_name)
+  bin_dir <- file.path(base_dir, "pgsql", "bin")
+  pg_ctl <- file.path(bin_dir, "pg_ctl.exe")
+
+  # Check if server is running
+  args <- c("-D", data_dir, "status")
+  status <- pg_ctl |>
+    system2(args = args, stdout = TRUE, stderr = TRUE) |>
+    suppressWarnings() # gives warning if server is not running
+
+  # Start server if not running
+  is_running <- "server is running" |>
+    grepl(paste(status, collapse = "\n"))
+  if (!is_running) {
+    log_step("Starting PostgreSQL server…")
+    args <- c("-D", data_dir, "-o", shQuote(paste0("-p ", port)), "start")
+    pg_ctl |>
+      system2(args = args, wait = TRUE)
+  }
+}
+
+connect_to_postgres_db <- function(
+  db_name = "mydb",
+  version = "15.3-1",
+  port = 5432,
+  user = "postgres",
+  password = "postgres"
+) {
+  # Start server if not running
+  db_name |>
+    start_postgres_server(
+      version = version,
+      port = port,
+      user = user,
+      password = password
+    )
+  # Wait for readiness
+  log_step("Waiting for PostgreSQL to accept connections…")
+  for (i in seq_len(15)) {
+    Sys.sleep(1)
+    ok <- {
+      con <- .dbi_drivers$postgresql[[1]]() |>
+        DBI::dbConnect(
+          host = "127.0.0.1",
+          port = port,
+          user = user,
+          password = password
+        )
+      DBI::dbDisconnect(con)
+      TRUE
+    } |>
+      on_error(.return = FALSE)
+
+    if (ok) {
+      break
+    }
+    if (i == 15) stop("Timeout: PostgreSQL did not become ready.")
+  }
+
+  .dbi_drivers$postgresql[[1]]() |>
+    DBI::dbConnect(
+      host = "127.0.0.1",
+      port = port,
+      user = user,
+      password = password
+    )
+}
