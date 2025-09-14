d3d7fde8615a8b3c5714136f1cf41b4c5e89cb35
B-Nilson <nilson.brayden@gmail.com>
2025-09-14 09:15:56 -0700

    flesh out tests

--- tests/testthat/test-create_database.R
+++ tests/testthat/test-create_database.R
@@ -1,11 +1,24 @@
 test_that("SQLite works", {
-  db_path <- create_database("__test.sqlite")
+  db_path <- create_database(name = "__test.sqlite", return_connection = FALSE)
   expect_true(file.exists(db_path))
   file.remove(db_path)
 })
 
 test_that("duckdb works", {
-  db_path <- create_database("__test.duckdb")
+  db_path <- create_database("__test.duckdb", return_connection = FALSE)
   expect_true(file.exists(db_path))
+  file.remove(db_path)
+})
+
+test_that("setting path works", {
+  db_path <- create_database("__test.sqlite", path = tempdir(), return_connection = FALSE)
+  expect_true(file.exists(db_path))
+  file.remove(db_path)
+})
+
+test_that("setting type works", {
+  db_path <- create_database("__test.sqlite", type = "duckdb", return_connection = FALSE)
+  expect_true(file.exists(db_path))
+  expect_equal(tools::file_ext(db_path), "duckdb")
   file.remove(db_path)
 })
