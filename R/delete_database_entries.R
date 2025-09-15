2802578e2b965beae20fa8a9971fdac64abe0b60
B-Nilson <nilson.brayden@gmail.com>
2025-09-14 22:39:56 -0700

    init function to delete rows from a databse table

---
+++ R/delete_database_entries.R
@@ -0,0 +1,24 @@
+delete_database_entries <- function(db, table_name, entry_keys) {
+  stopifnot(is.character(db) & length(db) == 1 | is_db_connection(db))
+  stopifnot(is.character(table_name) & length(table_name) == 1)
+  stopifnot(length(entry_keys) >= 1, !is.null(entry_keys))
+  rlang::check_installed("DBI")
+  rlang::check_installed("dbplyr")
+
+  if (is.character(db)) {
+    db <- db_conn_from_path(db)
+  }
+
+  entry_sql <- entry_keys |> 
+    as.data.frame() |>
+    dplyr::mutate(dplyr::across(dplyr::everything(), ~ paste0(dplyr::cur_column(), " = ", .))) |> 
+    tidyr::unite("entry_keys", sep = " AND ") |> 
+    dplyr::mutate(entry_keys = paste0("(", .data$entry_keys, ")")) |> 
+    dplyr::pull("entry_keys") |> 
+    paste(collapse = " OR ")
+    
+  delete_template <- "DELETE FROM %s WHERE %s"
+  delete_query <- delete_template |>
+    sprintf(table_name, entry_sql)
+  db |> DBI::dbExecute(delete_query)
+}
