23d060f53ba972ed879bb26f928caac6638cc4cf
B-Nilson <nilson.brayden@gmail.com>
2025-09-16 08:03:32 -0700

    Expend insert function to allow for appending

--- R/insert_file_lines.R
+++ R/insert_file_lines.R
@@ -1,27 +1,31 @@
 #' Insert new lines into a text-file inline
 #'
 #' @description
-#' Uses sed to insert select lines from a matrix/data.frame/vector into a file inline -
+#' Appends lines to a file (if `line_number = -1`), or uses sed to insert select lines from a matrix/data.frame/vector into a file inline -
 #' faster that loading the file into memory,
 #' then inserting the lines, and then writing the file back out.
 #'
 #' @param file_path The path to the file to modify.
-#' @param line_number The line number to insert the data at. Line numbers start at 1, which in most cases is the header line - ensure you don't insert the header by accident.
+#' @param line_number The line number to insert the data at.
+#'   If -1, the data will be appended to the end of the file. 
+#'   Line numbers start at 1, which in most cases is the header line - ensure you don't insert the header by accident.
 #' @param lines_to_insert A character vector of lines to insert, OR a matrix or data frame containing the data to insert.
 #'
 #' @return Invisibly returns `TRUE` if the file was editted successfully.
 #' @export
 insert_file_lines <- function(file_path, line_number, lines_to_insert) {
   stopifnot(is.character(file_path) & length(file_path) == 1)
-  stopifnot(is.numeric(line_number) & length(line_number) == 1)
+  stopifnot(is.numeric(line_number), length(line_number) == 1, line_number >= 1 | line_number == -1)
   stopifnot(
     is.character(lines_to_insert) |
       is.matrix(lines_to_insert) |
       is.data.frame(lines_to_insert)
   )
-  # Ensure sed is installed
-  # TODO: add fallback that is not inline
-  check_if_cmd_exists("sed")
+  # Ensure sed is installed if not appending
+  if (line_number != -1) {
+    # TODO: add fallback that is not inline
+    check_if_cmd_exists("sed")
+  }
 
   # Convert matrices to data.frames
   if (is.matrix(lines_to_insert)) {
@@ -38,14 +42,20 @@ insert_file_lines <- function(file_path, line_number, lines_to_insert) {
       dplyr::pull("line")
   }
 
-  # Write to temp file
-  temp_file <- tempfile()
-  writeLines(lines_to_insert, temp_file)
+  # Append to end if -1, otherwise use sed to insert
+  if (line_number == -1) {
+    result <- lines_to_insert |> 
+      write(file_path, sep = "\n", append = TRUE)
+  } else {
+    # Write to temp file
+    temp_file <- tempfile()
+    writeLines(lines_to_insert, temp_file)
 
-  # Build and run command
-  result <- 'sed -i "%sr %s" %s' |>
-    sprintf(line_number - 1, temp_file, file_path) |>
-    system(intern = TRUE)
+    # Build and run command
+    result <- 'sed -i "%sr %s" %s' |>
+      sprintf(line_number - 1, temp_file, file_path) |>
+      system(intern = TRUE)
+  }
 
   invisible(length(result) == 0)
 }
--- tests/testthat/test-insert_file_lines.R
+++ tests/testthat/test-insert_file_lines.R
@@ -23,3 +23,28 @@ test_that("basic case works", {
   # Cleanup
   file.remove(data_file_path)
 })
+
+test_that("append works", {
+  # Write out a test datafile
+  data_file_path <- tempfile()
+  test_data <- datasets::airquality
+  row.names(test_data) <- NULL
+  test_data |> write.csv(data_file_path, row.names = FALSE)
+
+  # Append 5 extra lines
+  new_lines <- test_data |> 
+    utils::tail(5) |> 
+    dplyr::mutate(Month = 12)
+  data_file_path |>
+    insert_file_lines(line_number = -1, lines_to_insert = new_lines)
+
+  # Check that worked
+  expected <- test_data |> rbind(new_lines) |> as.matrix()
+  row.names(expected) <- NULL
+  read.csv(data_file_path) |>
+    as.matrix() |> 
+    expect_equal(expected, tolerance = 0.0001)
+  
+  # Cleanup
+  file.remove(data_file_path)
+})
