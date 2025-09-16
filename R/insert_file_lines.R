0ec153b0200fe7f6b7daf2b8abce73aea349913e
B-Nilson <nilson.brayden@gmail.com>
2025-09-16 07:50:53 -0700

    document

--- R/insert_file_lines.R
+++ R/insert_file_lines.R
@@ -1,3 +1,16 @@
+#' Insert new lines into a text-file inline
+#'
+#' @description
+#' Uses sed to insert select lines from a matrix/data.frame/vector into a file inline -
+#' faster that loading the file into memory,
+#' then inserting the lines, and then writing the file back out.
+#'
+#' @param file_path The path to the file to modify.
+#' @param line_number The line number to insert the data at. Line numbers start at 1, which in most cases is the header line - ensure you don't insert the header by accident.
+#' @param lines_to_insert A character vector of lines to insert, OR a matrix or data frame containing the data to insert.
+#'
+#' @value Invisibly returns `TRUE` if the file was editted successfully.
+#' @export
 insert_file_lines <- function(file_path, line_number, lines_to_insert) {
   stopifnot(is.character(file_path) & length(file_path) == 1)
   stopifnot(is.numeric(line_number) & length(line_number) == 1)
