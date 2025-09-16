528f927cbc851f061ba4a08de3bf804c52863fb9
B-Nilson <nilson.brayden@gmail.com>
2025-09-15 19:12:18 -0700

    move from helpers to seperate file

---
+++ R/db_transaction.R
@@ -0,0 +1,28 @@
+# Run code block in a database transaction (rollback on error)
+db_transaction <- function(db, ...) {
+  # Begin a transaction
+  DBI::dbBegin(db)
+
+  # run code block passed to function, capture error as warning
+  transaction_failed <- ... |> 
+    on_error(.return = NULL, .warn = TRUE) |> 
+    is.null()
+
+  # Rollback the transaction and quit if failed
+  if (transaction_failed) {
+    DBI::dbRollback(db)
+    stop("Transaction failed.")
+  }
+
+  # Commit the transaction, capture error if needed
+  result <- DBI::dbCommit(db) |>
+    on_error(.return = NULL, .warn = TRUE)
+
+  # Rollback the transaction if failed
+  if (is.null(result)) {
+    DBI::dbRollback(db)
+    stop("Transaction failed.")
+  }
+
+  return(invisible(result))
+}
