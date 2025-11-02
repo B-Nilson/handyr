3ec8a9ceb83710c5fa3a3103ff262094ab83b0e5
B-Nilson <nilson.brayden@gmail.com>
2025-11-02 13:41:07 -0800

    allow null primary keys and cleanup db_create_table

--- R/write_to_database.R
+++ R/write_to_database.R
@@ -101,17 +101,15 @@ db_create_table <- function(
   db,
   table_name,
   new_data,
-  primary_keys,
-  unique_indexes = NULL
+  primary_keys = NULL,
+  unique_indexes = NULL,
+  insert_data = TRUE
 ) {
   if (is.character(db)) {
     db <- db_conn_from_path(db)
   }
-
-  # Build primary key SQL
-  primary_keys_safe <- primary_keys |> DBI::dbQuoteIdentifier(conn = db)
-  primary_key_sql <- "\tPRIMARY KEY (%s)" |>
-    sprintf(paste0(primary_keys_safe, collapse = ", "))
+  table_name_safe <- table_name |>
+    DBI::dbQuoteIdentifier(conn = db)
 
   # Build column definition SQL
   column_types <- db |>
@@ -119,6 +117,15 @@ db_create_table <- function(
   column_def_sql <- '\t"%s" %s' |>
     sprintf(names(new_data), column_types) |>
     paste(collapse = ",\n")
+
+  # Build primary key SQL
+  if (is.null(primary_keys)) {
+    primary_key_sql <- ""
+  } else {
+    primary_keys_safe <- primary_keys |> DBI::dbQuoteIdentifier(conn = db)
+    primary_key_sql <- "\tPRIMARY KEY (%s)" |>
+      sprintf(paste0(primary_keys_safe, collapse = ", "))
+  }
 
   # Build unique constraint SQL
   if (is.null(unique_indexes)) {
@@ -136,35 +143,26 @@ db_create_table <- function(
 
   # Build table creation query - defer constraints if postgresql for speed
   is_postgres <- inherits(db, "PqConnection") | inherits(db, "PostgreSQL")
-  create_template <- dplyr::case_when(
-    !is_postgres ~ ifelse(
-      is.null(unique_indexes),
-      "CREATE TABLE %s (\n%s,\n%s\n);",
-      "CREATE TABLE %s (\n%s,\n%s,\n%s\n);"
-    ),
-    is_postgres ~ "CREATE TABLE %s (%s);"
-  )
-
-  create_query <- create_template |>
+  create_query <- "CREATE TABLE %s (\n%s%s%s\n);" |>
     sprintf(
-      table_name |> DBI::dbQuoteIdentifier(conn = db),
+      table_name_safe,
       column_def_sql,
-      primary_key_sql,
-      unique_constraint_sql
-    ) |>
-    suppressWarnings() # handle no unique indexes
+      (is.null(primary_keys) | is_postgres) |>
+        ifelse("", paste(",\n", primary_key_sql)),
+      (is.null(unique_indexes) | is_postgres) |>
+        ifelse("", paste(",\n", unique_constraint_sql))
+    )
 
   # Create table
   db |> DBI::dbExecute(create_query)
 
   # insert rows if provided
-  if (nrow(new_data)) {
+  if (nrow(new_data) & insert_data) {
     success <- db |>
       DBI::dbWriteTable(
         value = new_data,
         name = table_name,
-        append = TRUE,
-        row.names = FALSE
+        append = TRUE
       )
     n_rows_inserted <- ifelse(success, nrow(new_data), 0)
   } else {
@@ -173,19 +171,14 @@ db_create_table <- function(
 
   # Add constraints if defered
   if (is_postgres) {
-    pkey_query <- "ALTER TABLE %s ADD PRIMARY KEY (%s);" |>
-      sprintf(
-        table_name |> DBI::dbQuoteIdentifier(conn = db),
-        primary_keys_safe |> paste0(collapse = ", ")
-      )
+    pkey_query <- "ALTER TABLE %s ADD %s;" |>
+      sprintf(table_name_safe, primary_key_sql)
     db |>
       DBI::dbExecute(pkey_query)
     if (!is.null(unique_indexes)) {
-      constraint_query <- "ALTER TABLE %s %s;" |>
-        sprintf(
-          table_name |> DBI::dbQuoteIdentifier(conn = db),
-          unique_constraint_sql
-        )
+      # TODO: test this
+      constraint_query <- "ALTER TABLE %s ADD CONSTRAINT %s;" |>
+        sprintf(table_name_safe, unique_constraint_sql)
       db |>
         DBI::dbExecute(constraint_query)
     }
