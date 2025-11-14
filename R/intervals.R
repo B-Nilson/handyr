d7d02b28e90ce15ad181cf0663ab6ae2597d56c1
B-Nilson <nilson.brayden@gmail.com>
2025-11-14 11:30:18 -0800

    add wrapper for creating intervals and extend seq() to work with Interval

---
+++ R/intervals.R
@@ -0,0 +1,75 @@
+#' Create an Interval object from a date range
+#' @description
+#' `Interval` objects represent a range of time and  can be created using `%--%` from the `lubridate` package.
+#' [as_interval()] makes this 'pipe friendly', allowing you to create an `Interval` object from a vector of 2 dates, datetimes, or characters
+#' (formatted as "YYYY-MM-DD HH:MM:SS" UTC time or "now" for the current time).
+#'
+#' @param date_range (Optional - required if `start` and `end` are not provided).
+#'   A vector of 2 dates, datetimes, or characters
+#'   (formatted as "YYYY-MM-DD HH:MM:SS" UTC time or "now" for the current time) representing the start and end time period.
+#'   NA values will be replaced with 1970-01-01 or "now" for the first and second values, respectively.
+#' @param start (Optional - required if `date_range` is not provided).
+#'   A date, datetime, or character (formatted as "YYYY-MM-DD HH:MM:SS" UTC time or "now" for the current time) representing the start of the date range.
+#' @param end (Optional - required if `date_range` is not provided).
+#'   A date, datetime, or character (formatted as "YYYY-MM-DD HH:MM:SS" UTC time or "now" for the current time) representing the end of the date range.
+#' @return An `Interval` object representing the date range.
+#' @export
+as_interval <- function(date_range = NULL, start = NULL, end = NULL) {
+  stopifnot(
+    "At least one of `date_range` or `start` and `end` must be provided." = !is.null(date_range) | !is.null(start) & !is.null(end)
+  )
+  # Handle date_range provided instead of start/end
+  if (!is.null(date_range)) {
+    start <- date_range[[1]]
+    end <- date_range[[2]]
+  }
+
+  # Handle non-date start/end
+  if ("character" %in% class(start) | "character" %in% class(end) | any(is.na(c(start, end)))) {
+    # TODO: vectorise check_date_range()
+    date_ranges <- start |>
+      for_each(.enumerate = TRUE, .bind = TRUE, \(value, i) {
+        check_date_range(c(start = value, end = end[[i]])) |>
+          as.list() |>
+          as.data.frame()
+      })
+    start <- date_ranges$start
+    end <- date_ranges$end
+  }
+  lubridate::`%--%`(start, end)
+}
+
+#' Extend seq() to work with Intervals
+#'
+#' @description
+#' `Interval` objects can be created using `%--%` from the `lubridate` package, or the handy wrapper [as_interval()].
+#' These objects represent a range of time, so it makes sense to be able to generate a sequence of dates within that range.
+#'
+#' [seq.Interval()] extends [seq()] to work with `Interval` objects.
+#'
+#' @param from A `Interval` object representing the date range.
+#' @param to Not used.
+#' @param by A string indicating the interval to use.
+#'   Can be one of "auto", "1 seconds", "1 minutes", "1 hours", or "1 days".
+#'   If "auto", the interval will be automatically determined based length of the interval.
+#'   Default is "auto".
+#' @return A sequence of dates within the interval.
+#' @export
+seq.Interval <- function(from, to = NULL, by = "auto") {
+  if (by == "auto") {
+    interval_length <- lubridate::int_length(from)
+    if (interval_length < 60) {
+      by <- "1 seconds"
+    } else if (interval_length < 3600) {
+      by <- "1 minutes"
+    } else if (interval_length < 86400) {
+      by <- "1 hours"
+    } else if (interval_length < 31536000) {
+      by <- "1 days"
+    } else {
+      by <- "1 years"
+    }
+  }
+  lubridate::int_start(from) |>
+    seq(lubridate::int_end(from), by = by)
+}
