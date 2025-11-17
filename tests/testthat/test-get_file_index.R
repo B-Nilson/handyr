853386487298bbde06b6ce70613b6977cd742ac8
B-Nilson <nilson.brayden@gmail.com>
2025-11-16 17:24:06 -0800

    add/export/test function to get a file listing from a default apache file index

---
+++ tests/testthat/test-get_file_index.R
@@ -0,0 +1,10 @@
+test_that("basic case works", {
+  url <- "https://aqmap.ca/aqmap/outputs/"
+  get_file_index(url) |> 
+    expect_no_error() |> 
+    expect_length(n = 4)
+
+  url <- "https://google.com"
+  get_file_index(url) |> 
+    expect_error()
+})
