03902a5cf50fe34ee74d914cd6c394beeeeb0f86
B-Nilson <nilson.brayden@gmail.com>
2025-11-20 21:25:18 -0800

    improve handling of x having units in convert_units

--- tests/testthat/test-convert_units.R
+++ tests/testthat/test-convert_units.R
@@ -10,4 +10,8 @@ test_that("keep_units works", {
     convert_units(c(1, 2, 3), from = "m", to = "km", keep_units = TRUE),
     c(0.001, 0.002, 0.003) |> units::set_units("km")
   )
+  expect_equal(
+    1:5 |> units::set_units("cm") |> convert_units(to = "m"),
+    (1:5 / 100) |> units::set_units("m")
+  )
 })
