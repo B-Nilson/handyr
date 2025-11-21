e4947523310359981aeb944acafb01a08a610e31
B-Nilson <nilson.brayden@gmail.com>
2025-11-21 10:29:24 -0800

    document and export create_database_table

--- R/create_database_table.R
+++ R/create_database_table.R
@@ -1,4 +1,22 @@
-# Create table if not already existing
+#' Create table if not already existing
+#'
+#' Create a table in a database if it doesn't already exist.
+#' This is alternative to [DBI::dbCreateTable()] with additional features for creating primary keys, unique indexes, and indexes.
+#'
+#' @param db A database connection or path to database (if sqlite or duckdb file extension).
+#' @param table_name A character string specifying the table to write to.
+#' @param new_data A data frame to write to the table.
+#' @param primary_keys A character vector of column names to use as the primary key, the main identifier of individual rows in a table.
+#'   Multiple columns can be specified and uniqueness will be assessed based on the combination of columns.
+#'   (e.g. `primary_keys = c("col1", "col2")` will add a primary key on the combination of `col1` and `col2`).
+#' @param unique_indexes A list of character vector(s) of column names to use as unique indexes.
+#'   These will be added to the table, in addition to the primary key, and will result in an error if non-unique data is inserted/existing.
+#'   Indexes speed up queries by allowing for faster lookups, but can increase the size of the database and reduce write performance.
+#' @param indexes A list of character vector(s) of column names to use as indexes.
+#' @param insert_data A logical indicating whether to insert the data frame provided in `new_data` into the created table.
+#'   If `FALSE`, the table will be created but no data will be inserted.
+#' @return A data frame containing the number of rows inserted into the table.
+#' @export
 create_database_table <- function(
   db,
   table_name,
