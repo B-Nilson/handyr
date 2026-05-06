220c17ef2beb4bc68c79db5f00691158b233dc1d
Brayden Nilson <nilson.brayden@gmail.com>
2026-05-06 13:41:25 -0700

    extend clamp to dates/times

--- R/clamp.R
+++ R/clamp.R
@@ -2,13 +2,13 @@
 #'
 #' `clamp` sets values less than `min(range)` to `min(range)` and values greater than `max(range)` to `max(range)`
 #'
-#' @param x A numeric vector.
-#' @param range A numeric vector of length two to clamp `x` to.
+#' @param x A numeric or date/POSIXct vector.
+#' @param range A numeric or date/POSIXct vector of length two to clamp `x` to.
 #'   * If `range[1]` is `NA` (default), then `min(x)` will be used.
 #'   * If `range[2]` is `NA` (default), then `max(x)` will be used.
 #'
 #' @return
-#' A numeric vector equal to `x` with:
+#' A numeric or date/POSIXct vector equal to `x` with:
 #'  * values less than `min(range)` set to `min(range)`
 #'  * values greater than `max(range)` set to `max(range)`
 #'
@@ -20,8 +20,16 @@
 #' clamp(1:5, range = c(-1, 1)) # 1, 1, 1, 1, 1
 clamp <- function(x, range = c(NA, NA)) {
   # Handle inputs
-  stopifnot(is.numeric(x), is.vector(x), !is.list(x))
-  stopifnot(length(range) == 2, is.numeric(range) | all(is.na(range)))
+  stopifnot(
+    is.numeric(x) || lubridate::is.Date(x) || lubridate::is.POSIXct(x)
+  )
+  stopifnot(
+    length(range) == 2,
+    is.numeric(range) ||
+      lubridate::is.Date(x) ||
+      lubridate::is.POSIXct(x) |
+      all(is.na(range))
+  )
 
   # set NA range values to min/max
   if (is.na(range[1])) {
--- tests/testthat/test-clamp.R
+++ tests/testthat/test-clamp.R
@@ -19,3 +19,32 @@ test_that("NA range values work", {
     expected = c(2, 2:5)
   )
 })
+
+test_that("dates/times work", {
+  dates <- seq(as.Date("2022-01-01"), as.Date("2022-01-05"))
+  clamp_to <- c(as.Date("2022-01-03"), as.Date("2022-01-04"))
+  expected <- dates
+  expected[dates < clamp_to[1]] <- clamp_to[1]
+  expected[dates > clamp_to[2]] <- clamp_to[2]
+  expect_equal(
+    clamp(dates, range = clamp_to),
+    expected = c(clamp_to[1], clamp_to[1], dates[3:4], clamp_to[2])
+  )
+
+  datetimes <- seq(
+    as.POSIXct("2022-01-01 00:00:00"),
+    as.POSIXct("2022-01-05 00:00:00"),
+    by = "12 hours"
+  )
+  clamp_to <- c(
+    as.POSIXct("2022-01-03 00:00:00"),
+    as.POSIXct("2022-01-04 00:00:00")
+  )
+  expected <- datetimes
+  expected[datetimes < clamp_to[1]] <- clamp_to[1]
+  expected[datetimes > clamp_to[2]] <- clamp_to[2]
+  expect_equal(
+    clamp(datetimes, range = clamp_to),
+    expected = expected
+  )
+})
